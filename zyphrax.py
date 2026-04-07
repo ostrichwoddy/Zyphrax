51c38b1372190ae5374a688fe81c86f46b643a85
ostrichwoddy <swapnilacharjee1724@gmail.com>
2026-04-07 19:22:00 +0530

    command line arguments with argparse

--- zyphrax.py
+++ zyphrax.py
@@ -1,15 +1,23 @@
-import sys
+import argparse
 from vt_scan import scanfile
 from metadata import get_file_metadata
 
-file_path = sys.argv[1]
-# scanfile(file_path)
+parser = argparse.ArgumentParser(description="Analyze a file locally and optionally with VirusTotal")
+parser.add_argument("file", help="Path to the file to analyze")
+parser.add_argument("--vt", action="store_true", help="Perform VirusTotal scan")
+args = parser.parse_args()
+
+file_path = args.file
+
 metadata = get_file_metadata(file_path)
+
+print("\nFILE METADATA\n-------------")
 for key, value in metadata.items():
     print(f"{key.capitalize().replace('_', ' ')}: {value}")
 
-vt_results = scanfile(file_path)
-if vt_results:
-    print("\nVirusTotal Scan Summary:")
-    for key, value in vt_results.items():
-        print(f"{key}: {value}")
+if args.vt:
+    vt_results = scanfile(file_path)
+    if vt_results:
+        print("\nVIRUSTOTAL SCAN SUMMARY\n-------------")
+        for key, value in vt_results.items():
+            print(f"{key}: {value}")
